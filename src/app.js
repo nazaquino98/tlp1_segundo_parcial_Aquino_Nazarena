0b81cb915dde7c80e2f212d1f3f1a130bc2b1544
nazaquino98 <nazarenaayelen49@gmail.com>
2024-06-27 14:57:23 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const db = [
+    {
+        "id": 1,
+        "nombre": "remera",
+        "cantidad": parseInt(5),
+        "precio": parseFloat(2800)
+    },
+    {
+        "id": 2,
+        "nombre": "jean",
+        "cantidad": parseInt(8),
+        "precio": parseFloat(8500)
+    },
+    {
+        "id": 3,
+        "nombre": "campera",
+        "cantidad": parseInt(3),
+        "precio": parseFloat(10200)
+    },
+]
